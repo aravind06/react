1eee8418011171305e6faad4c65b1aa08c9cdab2
aravind06 <aravindkumar0603@gmail.com>
2019-12-31 15:04:41 +0530

    Updates

---
+++ precache-manifest.f179637affbc0d44f35ac96a7b233205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9d29ead7ebea727e7545824c1089ed",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ca495937ba53f19fd1e1",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53551e135ea48a20c175",
+    "url": "/react/static/js/2.f8a32417.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.f8a32417.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca495937ba53f19fd1e1",
+    "url": "/react/static/js/main.99b73b72.chunk.js"
+  },
+  {
+    "revision": "53873e8f0a12c74a5d6a",
+    "url": "/react/static/js/runtime-main.8b45e6fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
